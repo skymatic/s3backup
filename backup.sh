8b03f02d256acce83a53f2bd70b8125024418cdb
Sebastian Stenzel <overheadhunter@users.noreply.github.com>
2018-06-06 18:13:28 +0200

    Update backup.sh
    Fixing target path

--- backup.sh
+++ backup.sh
@@ -34,12 +34,13 @@ for FILE in ${BACKUP_FILE_GLOB}
 do
   if [ -z "${OPENSSL_ENC_PASS}" ]
   then
-    echo "OPENSSL_ENC_PASS not set. Uploading unencrypted file ${FILE}...";
-    aws s3 cp ${FILE} ${AWS_S3_BUCKET_PATH}/${FILE} --storage-class=${AWS_STORAGE_CLASS};
+    echo "Uploading unencrypted file ${FILE}...";
+    aws s3 cp ${FILE} ${AWS_S3_BUCKET_PATH}${FILE} --storage-class=${AWS_STORAGE_CLASS};
   else \
-    echo "Encrypting file  ${FILE} using $(openssl version) with AES-128-CTR and iv/key derived from provided password. Saving to /tmp/${FILE}.enc";
+    echo "Encrypting file  ${FILE} using $(openssl version) with AES-128-CTR and iv/key derived from provided password.";
     openssl enc -AES-128-CTR -in ${FILE} -out /tmp/ENC_FILE -pass pass:${OPENSSL_ENC_PASS};
-    aws s3 cp /tmp/${FILE}.enc ${AWS_S3_BUCKET_PATH}/${FILE}.enc --storage-class=${AWS_STORAGE_CLASS};
+    echo "Uploading encrypted file ${FILE}.enc...";
+    aws s3 cp /tmp/${FILE}.enc ${AWS_S3_BUCKET_PATH}${FILE}.enc --storage-class=${AWS_STORAGE_CLASS};
     rm /tmp/${FILE}.enc;
   fi
 done
